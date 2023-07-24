f8e695a9371dd474c895137ee3eda80871192e62
bluepadu <padum3955@gmail.com>
2023-07-24 11:36:10 -0700

    Add: folding inside content streams

--- client/src/PDFFoldingRangeProvider.ts
+++ client/src/PDFFoldingRangeProvider.ts
@@ -8,6 +8,11 @@ export class PDFFoldingRangeProvider implements FoldingRangeProvider {
 		let startBracketLine: number = -1;
 		let startStreamLine: number = -1;
 		let startPdfNameLine: number = -1;
+		let startQLine: number = -1;
+		let startBTLine: number = -1;
+		let startBXLine: number = -1;
+		let startBDCLine: number = -1;
+		let startBMCLine: number = -1;
 
 		for (let i = 0; i < document.lineCount; i++) {
 			let line = document.lineAt(i).text.trim();
@@ -45,6 +50,46 @@ export class PDFFoldingRangeProvider implements FoldingRangeProvider {
 				let r = new FoldingRange(startPdfNameLine, i);
 				ranges.push(r);
 				startPdfNameLine = -1;
+			}
+
+			if (line.startsWith('q')) {
+				startQLine = i;
+			} else if (line.startsWith('Q') && startQLine >= 0) {
+				let r = new FoldingRange(startQLine, i);
+				ranges.push(r);
+				startQLine = -1;
+			}
+
+			if (line.startsWith('BT')) {
+				startBTLine = i;
+			} else if (line.startsWith('ET') && startBTLine >= 0) {
+				let r = new FoldingRange(startBTLine, i);
+				ranges.push(r);
+				startBTLine = -1;
+			}
+
+			if (line.startsWith('BX')) {
+				startBXLine = i;
+			} else if (line.startsWith('EX') && startBXLine >= 0) {
+				let r = new FoldingRange(startBXLine, i);
+				ranges.push(r);
+				startBXLine = -1;
+			}
+
+			if (line.startsWith('BDC')) {
+				startBDCLine = i;
+			} else if (line.startsWith('BMC')) {
+				startBMCLine = i;
+			} else if (line.startsWith('EMC')) {
+				if (startBDCLine >= 0) {
+					let r = new FoldingRange(startBDCLine, i);
+					ranges.push(r);
+					startBDCLine = -1;
+				} else if (startBMCLine >= 0) {
+					let r = new FoldingRange(startBMCLine, i);
+					ranges.push(r);
+					startBMCLine = -1;
+				}
 			}
 		}
 		return ranges;
