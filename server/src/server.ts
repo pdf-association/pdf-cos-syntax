7d8b12be4d800b8291394d1b317c1be526e487b0
bluepadu <padum3955@gmail.com>
2023-08-31 18:32:23 -0700

    Fix: for caching

--- server/src/server.ts
+++ server/src/server.ts
@@ -32,7 +32,7 @@ import {
   isFilePDF,
   getSemanticTokenAtPosition,
   computeDefinitionLocationForToken,
-  XrefInfoMatrix
+  XrefInfoMatrix,
 } from "./pdfUtils";
 
 // for server debug.
@@ -44,40 +44,55 @@ if (process.env.NODE_ENV === "development") {
   // require("source-map-support").install();
 }
 
-// console.log('server debugging');
 // Create a connection for the server, using Node's IPC as a transport.
 // Also include all preview / proposed LSP features.
 const connection = createConnection(ProposedFeatures.all);
 
 // Create a simple text document manager.
-const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+const documents = new TextDocuments(TextDocument);
 
 let hasConfigurationCapability = false;
 let hasWorkspaceFolderCapability = false;
 let hasDiagnosticRelatedInformationCapability = false;
 
 const tokenTypes = ["indirectReference", "indirectObject", "xrefTableEntry"]; // ... add other token types as needed
-const tokenModifiers = ["deprecated"]; // ... add other token modifiers as needed
+// const tokenModifiers = ["deprecated"]; // ... add other token modifiers as needed
 
-class SemanticTokensBuilder {
-  private _data: number[] = [];
-
-  public push(
-    line: number,
-    char: number,
-    length: number,
-    tokenType: number,
-    tokenModifier: number
-  ) {
-    this._data.push(line, char, length, tokenType, tokenModifier);
-  }
-
-  public build(): any {
-    return {
-      data: this._data,
-    };
-  }
+// The example settings
+interface ExampleSettings {
+  maxNumberOfProblems: number;
 }
+
+interface SimpleTextDocument {
+  getText(): string;
+}
+// The global settings, used when the `workspace/configuration` request is not supported by the client.
+// Please note that this is not the case when using this server with the client provided in this example
+// but could happen with other clients.
+const defaultSettings: ExampleSettings = { maxNumberOfProblems: 100 };
+let globalSettings: ExampleSettings = defaultSettings;
+
+// Cache the settings of all open documents
+type DocumentData = {
+  settings: ExampleSettings;
+  xrefMatrix?: XrefInfoMatrix;
+};
+// const documentSettings: Map<string, Thenable<ExampleSettings>> = new Map();
+const documentSettings: Map<string, DocumentData> = new Map();
+
+documents.onDidChangeContent((change) => {
+  const document = change.document;
+  if (document) {
+    updateXrefMatrixForDocument(document.uri, document.getText());
+  }
+});
+
+connection.onDidOpenTextDocument((params) => {
+  const document = documents.get(params.textDocument.uri);
+  if (document) {
+    updateXrefMatrixForDocument(document.uri, document.getText());
+  }
+});
 
 connection.onInitialize((params: InitializeParams) => {
   const capabilities = params.capabilities;
@@ -149,45 +164,6 @@ connection.onRequest("textDocument/semanticTokens/full", (params) => {
   return tokenizeDocument(document);
 });
 
-function tokenizeDocument(document: TextDocument): any {
-  const tokensBuilder = new SemanticTokensBuilder();
-  const lines = document.getText().split(/\r?\n/);
-
-  for (let line = 0; line < lines.length; line++) {
-    const currentLine = lines[line];
-    const referenceMatch = currentLine.match(/(\d+) (\d+) R/);
-    if (referenceMatch) {
-      const startChar = referenceMatch.index!;
-      const length = referenceMatch[0].length;
-      tokensBuilder.push(
-        line,
-        startChar,
-        length,
-        tokenTypes.indexOf("indirectReference"),
-        0
-      ); // assuming no modifier
-    }
-
-    // ... other token matchers ...
-  }
-
-  return tokensBuilder.build();
-}
-
-// The example settings
-interface ExampleSettings {
-  maxNumberOfProblems: number;
-}
-
-// The global settings, used when the `workspace/configuration` request is not supported by the client.
-// Please note that this is not the case when using this server with the client provided in this example
-// but could happen with other clients.
-const defaultSettings: ExampleSettings = { maxNumberOfProblems: 100 };
-let globalSettings: ExampleSettings = defaultSettings;
-
-// Cache the settings of all open documents
-const documentSettings: Map<string, Thenable<ExampleSettings>> = new Map();
-
 connection.onDidChangeConfiguration((change) => {
   if (hasConfigurationCapability) {
     // Reset all cached document settings
@@ -201,21 +177,6 @@ connection.onDidChangeConfiguration((change) => {
   // Revalidate all open text documents
   documents.all().forEach(validateTextDocument);
 });
-
-function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
-  if (!hasConfigurationCapability) {
-    return Promise.resolve(globalSettings);
-  }
-  let result = documentSettings.get(resource);
-  if (!result) {
-    result = connection.workspace.getConfiguration({
-      scopeUri: resource,
-      section: "pdf-cos-syntax",
-    });
-    documentSettings.set(resource, result);
-  }
-  return result;
-}
 
 // Only keep settings for open documents
 documents.onDidClose((e) => {
@@ -234,6 +195,203 @@ documents.onDidClose((e) => {
 documents.onDidChangeContent((change) => {
   validateTextDocument(change.document);
 });
+
+connection.onDidChangeWatchedFiles((_change) => {
+  // Monitored files have change in VSCode
+  connection.console.log("We received an file change event");
+});
+
+// This handler provides the initial list of the completion items.
+connection.onCompletion(
+  (_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+    // The pass parameter contains the position of the text document in
+    // which code complete got requested. For the example we ignore this
+    // info and always provide the same completion items.
+    return [
+      {
+        label: "TypeScript",
+        kind: CompletionItemKind.Text,
+        data: 1,
+      },
+      {
+        label: "JavaScript",
+        kind: CompletionItemKind.Text,
+        data: 2,
+      },
+    ];
+  }
+);
+
+// This handler resolves additional information for the item selected in
+// the completion list.
+connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
+  if (item.data === 1) {
+    item.detail = "TypeScript details";
+    item.documentation = "TypeScript documentation";
+  } else if (item.data === 2) {
+    item.detail = "JavaScript details";
+    item.documentation = "JavaScript documentation";
+  }
+  return item;
+});
+
+/**
+ *  "Go to definition" for "X Y R" and in-use ("n") cross reference table entries
+ */
+connection.onDefinition(
+  (params: TextDocumentPositionParams): Definition | null => {
+    const { textDocument, position } = params;
+    // Get the document corresponding to the URI
+    const document = documents.get(textDocument.uri);
+    if (!document) return null;
+
+    // Get conventional xref tables
+    const xrefTables = extractAllXrefTables(document);
+    if (!xrefTables.length) {
+      return null;
+    }
+    // Fetch the semantic token at the given position
+    const tokenInfo = getSemanticTokenAtPosition(document, position);
+    // If no semantic token is found, return null
+    if (!tokenInfo) return null;
+
+    // Use the semantic token information to decide where the cursor should jump to.
+    // This could be based on the token's type, range, or other properties.
+    let targetLocation: Location | null = null;
+    for (const xrefTable of xrefTables) {
+      targetLocation = computeDefinitionLocationForToken(
+        tokenInfo,
+        document,
+        xrefTable
+      );
+
+      if (targetLocation) {
+        break;
+      }
+    }
+
+    return targetLocation;
+  }
+);
+
+connection.onReferences((params): Location[] | null => {
+  const document = documents.get(params.textDocument.uri);
+  if (!document) return null;
+
+  const position = params.position;
+  const token = getSemanticTokenAtPosition(document, position);
+
+  if (!token) return null;
+
+  let objectNumber: number;
+  let genNumber: number;
+  const lineText = document.getText(token.range);
+
+  switch (token.type) {
+    case "indirectReference": // X Y R
+    case "indirectObject": {
+      // X Y obj
+      const objMatch = lineText.match(/(\d+) (\d+)/);
+      if (!objMatch) return null;
+
+      objectNumber = parseInt(objMatch[1]);
+      genNumber = parseInt(objMatch[2]);
+      break;
+    }
+    case "xrefTableEntry": {
+      // only for in-use entries!
+      const match = lineText.match(/\b(\d{10}) (\d{5}) n\b/);
+      if (!match) return null;
+
+      const offset = parseInt(match[1]);
+      genNumber = parseInt(match[2]);
+      const xRefinfo = new XrefInfoMatrix();
+      xRefinfo.mergeAllXrefTables(document);
+      objectNumber = xRefinfo.getObjectNumberBasedOnByteOffset(
+        offset,
+        genNumber,
+        "n"
+      );
+      if (objectNumber === -1) return null;
+      break;
+    }
+    default:
+      return null;
+  }
+
+  return findAllReferences(objectNumber, genNumber, document);
+});
+
+connection.onHover((params): Hover | null => {
+  const docData = documentSettings.get(params.textDocument.uri);
+  const document = documents.get(params.textDocument.uri);
+  if (!docData || !docData.xrefMatrix || !document) return null;
+
+  const position = params.position;
+  const token = getSemanticTokenAtPosition(document, position);
+
+  if (!token) return null;
+
+  const lineText = document.getText(token.range);
+  const xRefInfo = docData.xrefMatrix;
+
+  // xRefInfo.mergeAllXrefTables(docData);
+
+  switch (token.type) {
+    case "xrefTableEntry": {
+      const match = lineText.match(/\b(\d{10}) (\d{5}) (n|f)\b/);
+      if (!match) return null;
+
+      const offset = parseInt(match[1]);
+      const genNum = parseInt(match[2]);
+      const flag = match[3];
+      const objNum = xRefInfo.getObjectNumberBasedOnByteOffset(
+        offset,
+        genNum,
+        flag
+      );
+      if (flag === "n") {
+        return { contents: `Object ${objNum} is at byte offset ${offset}` };
+      } else {
+        return { contents: `Object ${objNum} is a free object` };
+      }
+      break;
+    }
+    case "indirectReference": // X Y R
+    case "indirectObject": // X Y obj
+    default:
+      break;
+  }
+
+  return null;
+});
+
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+
+// Listen on the connection
+connection.listen();
+
+class SemanticTokensBuilder {
+  private _data: number[] = [];
+
+  public push(
+    line: number,
+    char: number,
+    length: number,
+    tokenType: number,
+    tokenModifier: number
+  ) {
+    this._data.push(line, char, length, tokenType, tokenModifier);
+  }
+
+  public build(): any {
+    return {
+      data: this._data,
+    };
+  }
+}
 
 /**
  * Perform basic validation of a conventional PDF:
@@ -437,168 +595,94 @@ async function validateTextDocument(textDocument: TextDocument): Promise<void> {
   connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
 }
 
-connection.onDidChangeWatchedFiles((_change) => {
-  // Monitored files have change in VSCode
-  connection.console.log("We received an file change event");
-});
+function tokenizeDocument(document: TextDocument): any {
+  const tokensBuilder = new SemanticTokensBuilder();
+  const lines = document.getText().split(/\r?\n/);
 
-// This handler provides the initial list of the completion items.
-connection.onCompletion(
-  (_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-    // The pass parameter contains the position of the text document in
-    // which code complete got requested. For the example we ignore this
-    // info and always provide the same completion items.
-    return [
-      {
-        label: "TypeScript",
-        kind: CompletionItemKind.Text,
-        data: 1,
-      },
-      {
-        label: "JavaScript",
-        kind: CompletionItemKind.Text,
-        data: 2,
-      },
-    ];
-  }
-);
-
-// This handler resolves additional information for the item selected in
-// the completion list.
-connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
-  if (item.data === 1) {
-    item.detail = "TypeScript details";
-    item.documentation = "TypeScript documentation";
-  } else if (item.data === 2) {
-    item.detail = "JavaScript details";
-    item.documentation = "JavaScript documentation";
-  }
-  return item;
-});
-
-/**
- *  "Go to definition" for "X Y R" and in-use ("n") cross reference table entries
- */
-connection.onDefinition(
-  (params: TextDocumentPositionParams): Definition | null => {
-    const { textDocument, position } = params;
-    // Get the document corresponding to the URI
-    const document = documents.get(textDocument.uri);
-    if (!document) return null;
-
-    // Get conventional xref tables
-    const xrefTables = extractAllXrefTables(document);
-    if (!xrefTables.length) {
-      return null;
-    }
-    // Fetch the semantic token at the given position
-    const tokenInfo = getSemanticTokenAtPosition(document, position);
-    // If no semantic token is found, return null
-    if (!tokenInfo) return null;
-
-    // Use the semantic token information to decide where the cursor should jump to.
-    // This could be based on the token's type, range, or other properties.
-    let targetLocation: Location | null = null;
-    for (const xrefTable of xrefTables) {
-      targetLocation = computeDefinitionLocationForToken(
-        tokenInfo,
-        document,
-        xrefTable
-      );
-
-      if (targetLocation) {
-        break;
-      }
+  for (let line = 0; line < lines.length; line++) {
+    const currentLine = lines[line];
+    const referenceMatch = currentLine.match(/(\d+) (\d+) R/);
+    if (referenceMatch) {
+      const startChar = referenceMatch.index!;
+      const length = referenceMatch[0].length;
+      tokensBuilder.push(
+        line,
+        startChar,
+        length,
+        tokenTypes.indexOf("indirectReference"),
+        0
+      ); // assuming no modifier
     }
 
-    return targetLocation;
-  }
-);
-
-connection.onReferences((params): Location[] | null => {
-  const document = documents.get(params.textDocument.uri);
-  if (!document) return null;
-
-  const position = params.position;
-  const token = getSemanticTokenAtPosition(document, position);
-
-  if (!token) return null;
-
-  let objectNumber: number;
-  let genNumber: number;
-  const lineText = document.getText(token.range);
-
-  switch (token.type) {
-    case "indirectReference": // X Y R
-    case "indirectObject": {  // X Y obj
-      const objMatch = lineText.match(/(\d+) (\d+)/);
-      if (!objMatch) return null;
-
-      objectNumber = parseInt(objMatch[1]);
-      genNumber = parseInt(objMatch[2]);
-      break;
-    }
-    case "xrefTableEntry": { // only for in-use entries! 
-      const match = lineText.match(/\b(\d{10}) (\d{5}) n\b/);
-      if (!match) return null;
-
-      const offset = parseInt(match[1]);
-      genNumber = parseInt(match[2]);
-      const xRefinfo = new XrefInfoMatrix;
-      xRefinfo.mergeAllXrefTables(document);
-      objectNumber = xRefinfo.getObjectNumberBasedOnByteOffset(offset, genNumber, "n");
-      if (objectNumber === -1) return null;
-      break;
-    }
-    default:
-      return null;
+    // ... other token matchers ...
   }
 
-  return findAllReferences(objectNumber, genNumber, document);
-});
+  return tokensBuilder.build();
+}
 
-connection.onHover((params): Hover | null => {
-  const document = documents.get(params.textDocument.uri);
-  if (!document) return null;
+async function getDocumentSettings(resource: string): Promise<DocumentData> {
+  // if (!hasConfigurationCapability) {
+  //   return Promise.resolve(globalSettings);
+  // }
+  // let result = documentSettings.get(resource);
+  // if (!result) {
+  //   result = connection.workspace.getConfiguration({
+  //     scopeUri: resource,
+  //     section: "pdf-cos-syntax",
+  //   });
+  //   documentSettings.set(resource, result);
+  // }
+  // return result;
+  const currentData = documentSettings.get(resource) || {
+    settings: globalSettings,
+  };
+  const newSettings = await connection.workspace.getConfiguration({
+    scopeUri: resource,
+    section: "pdf-cos-syntax",
+  });
+  documentSettings.set(resource, { ...currentData, settings: newSettings });
+  return { ...currentData, settings: newSettings };
+}
 
-  const position = params.position;
-  const token = getSemanticTokenAtPosition(document, position);
-
-  if (!token) return null;
-
-  const lineText = document.getText(token.range);
-  const xRefinfo = new XrefInfoMatrix;
-  console.log(xRefinfo);
-  xRefinfo.mergeAllXrefTables(document);
-
-  switch (token.type) {
-    case "xrefTableEntry": {
-      const match = lineText.match(/\b(\d{10}) (\d{5}) (n|f)\b/);
-      if (!match) return null;
-
-      const offset = parseInt(match[1]);
-      const genNum = parseInt(match[2]);
-      const flag = match[3];
-      const objNum = xRefinfo.getObjectNumberBasedOnByteOffset(offset, genNum, flag);
-      if (flag === "n") {
-        return { contents: `Object ${objNum} is at byte offset ${offset}` };
-      } else {
-        return { contents: `Object ${objNum} is a free object` };
-      }
-      break;
-    }
-    case "indirectReference": // X Y R
-    case "indirectObject": // X Y obj
-    default:
-      break;
+function updateXrefMatrixForDocument(uri: string, content: string) {
+  let docData = documentSettings.get(uri);
+  if (!docData) {
+    docData = { settings: globalSettings }; // or fetch default settings
+    documentSettings.set(uri, docData);
   }
 
-  return null;
-});
+  // Create or update the XrefInfoMatrix for the document content
+  docData.xrefMatrix = buildXrefMatrix(content);
+}
 
-// Make the text document manager listen on the connection
-// for open, change and close text document events
-documents.listen(connection);
+function buildXrefMatrix(content: string): XrefInfoMatrix {
+  // Create a new instance of the XrefInfoMatrix
+  const xrefMatrix = new XrefInfoMatrix();
 
-// Listen on the connection
-connection.listen();
+  const mockTextDocument: TextDocument = {
+    getText: () => content,
+    uri: "mockURI", // mock URI
+    languageId: "plaintext", // or any language ID you want to mock
+    version: 1, // mock version
+    positionAt: (offset: number) => {
+      // Mock implementation; adjust if necessary
+      return { line: 0, character: offset };
+    },
+    offsetAt: (position: Position) => {
+      // Mock implementation; adjust if necessary
+      return position.character;
+    },
+    lineCount: content.split("\n").length,
+    // Any other properties or methods from TextDocument should be added here in a similar fashion.
+  };
+
+  // Merge all xref tables found in the document into the matrix
+  const diagnostics = xrefMatrix.mergeAllXrefTables(mockTextDocument);
+
+  // In real-world use, you might want to handle diagnostics, e.g., log them or display them to users.
+  for (const diag of diagnostics) {
+    console.error(`[PDF Diagnostics] ${diag.message}`);
+  }
+
+  return xrefMatrix;
+}
