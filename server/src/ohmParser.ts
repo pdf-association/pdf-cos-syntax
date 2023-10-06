abdd14963d35919e4640b3498af1f30e68801888
petervwyatt <26521615+petervwyatt@users.noreply.github.com>
2023-10-06 23:59:29 +1100

    Switch to line-by-line tokenizer

--- server/src/ohmParser.ts
+++ server/src/ohmParser.ts
@@ -3,7 +3,7 @@ import * as fs from "fs";
 import { Token } from "./types";
 import * as path from "path";
 
-const grammarPath = path.join(__dirname, "../src/grammar/grammar_pdfFile.ohm");
+const grammarPath = path.join(__dirname, "../src/grammar/grammar_pdfTokens.ohm");
 const grammarString = fs.readFileSync(grammarPath, "utf-8");
 const grammar = ohm.grammar(grammarString);
 
@@ -29,124 +29,47 @@ function parse(text: string, startRule?: string): ParseResult {
   }
 }
 
+// Main entry point to Ohm parser called by LSP server
 function getTokens(text: string): Token[] {
   const tokenList: Token[] = [];
-  const matchResult = grammar.match(text);
-  if (!matchResult.succeeded()) {
-    console.log(`getTokens(): match did not succeed!`);
-    return [];
-  }
-
   const semantics = grammar.createSemantics();
-  // Example semantic actions
-  semantics.addOperation("extract", {
-    pdf(header, binary_marker, revision) {
-      console.log("PDF rule entered");
-      const token = {
-        start: this.source.startIdx,
-        end: this.source.endIdx,
-        type: "pdf",
-      };
-      tokenList.push(token);
 
-      header.extract();
-      binary_marker.extract();
-      revision.extract();
-      // body.extract();
-      // indirect_object_start.extract();
-      // indirect_object_end.extract();
-      // stream.extract();
-      // object.extract();
-      // dictionary.extract();
-      // key_value_pair.extract();
-      // array.extract();
-      // name.extract();
-      // name_hex_escape.extract();
-      // string.extract();
-      // string_literal.extract();
-      // string_literal_escape.extract();
-      // octal.extract();
-      // octal_digit.extract();
-      // escaped_eol.extract();
-      // hex_string.extract();
-      // hex_digit.extract();
-      // endobj.extract();
-      // indirect_ref.extract();
-      // number.extract();
-      // integer.extract();
-      // real.extract();
-      // bool.extract();
-      // null.extract();
-      // xref.extract();
-      // xref_subsection.extract();
-      // xref_subsection_marker.extract();
-      // xref_10entry.extract();
-      // xref_5entry.extract();
-      // xref_entry.extract();
-      // trailer.extract();
-      // startxref.extract();
-      // eof.extract();
-      // comment.extract();
-      // eol.extract();
-      // delimiter.extract();
-      // start_delimiter.extract();
-      // end_delimiter.extract();
-      // ws_incl_eol.extract();
-      // ws_no_eol.extract();
+  semantics.addOperation("extract", {
+    _iter(...children) {
+      console.log(children);
+      // return children.map(c => c.myOperation());
     },
-    header(_1, _2, _3, _4, _5, _6) {
-      console.log("Header rule entered");
+    header(_1, _2, _3, _4, _5) {
+      console.log("header rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
-        type: "header",
+        type: "%PDF-x.y",
       };
       tokenList.push(token);
     },
-    binary_marker(_1, _2, _3, _4, _5, _6, _7) {
+    indirect_object_start(objNum, _2, genNum, _4, objectName) {
+      console.log("indirect_object_start rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
-        type: "binary_marker",
-      };
-      tokenList.push(token);
-    },
-    revision(_1, _2, _3, _4, _5) {
-      const token = {
-        start: this.source.startIdx,
-        end: this.source.endIdx,
-        type: "revision",
-      };
-      tokenList.push(token);
-    },
-    body(elements) {
-      const token = {
-        start: this.source.startIdx,
-        end: this.source.endIdx,
-        type: "body",
-        content: elements.sourceString,
-      };
-      tokenList.push(token);
-    },
-    indirect_object_start(_1, objNum, _2, genNum, objectName, _3, _4) {
-      const token = {
-        start: this.source.startIdx,
-        end: this.source.endIdx,
-        type: "indirect_object_start",
+        type: "X Y obj",
         objNum: parseInt(objNum.sourceString),
         genNum: parseInt(genNum.sourceString),
       };
       tokenList.push(token);
     },
-    indirect_object_end(_1, _2, _3) {
+    endobj(_1) {
+      console.log("endobj rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
-        type: "indirect_object_end",
+        type: "endobj",
       };
       tokenList.push(token);
     },
-    stream(_1, _2, content, _3, _4, _5, _6) {
+    stream(_1, content, _3, _4, _5) {
+      console.log("stream rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
@@ -155,55 +78,44 @@ function getTokens(text: string): Token[] {
       };
       tokenList.push(token);
     },
-    object(content) {
+    dict_start(_1) {
+      console.log("dict_start rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
-        type: "object",
-        content: content.sourceString,
+        type: "<<"
       };
       tokenList.push(token);
     },
-    dictionary(_1, keyValues, _2, _3, _4) {
+    dict_end(_1) {
+      console.log("dict_end rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
-        type: "dictionary",
-        content: keyValues.sourceString,
+        type: ">>"
       };
       tokenList.push(token);
     },
-    key_value_pair(_1, key, _2, value) {
+    array_start(_1) {
+      console.log("array_start rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
-        type: "key_value_pair",
-        key: key.sourceString,
-        value: value.sourceString,
+        type: "["
       };
       tokenList.push(token);
     },
-    name_hex_escape(_1, hexDigit1, hexDigit2) {
+    array_end(_1) {
+      console.log("array_end rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
-        type: "name_hex_escape",
-        content: String.fromCharCode(
-          parseInt(hexDigit1.sourceString + hexDigit2.sourceString, 16)
-        ),
-      };
-      tokenList.push(token);
-    },
-    string(strContent) {
-      const token = {
-        start: this.source.startIdx,
-        end: this.source.endIdx,
-        type: "string",
-        content: strContent.extract(),
+        type: "]"
       };
       tokenList.push(token);
     },
     string_literal(_1, content, _2) {
+      console.log("string_literal rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
@@ -212,42 +124,8 @@ function getTokens(text: string): Token[] {
       };
       tokenList.push(token);
     },
-    string_literal_escape(content) {
-      const token = {
-        start: this.source.startIdx,
-        end: this.source.endIdx,
-        type: "string_literal_escape",
-        content: content.sourceString,
-      };
-      tokenList.push(token);
-    },
-    octal(_1, octalDigits, _2, _3) {
-      const token = {
-        start: this.source.startIdx,
-        end: this.source.endIdx,
-        type: "octal",
-        value: parseInt(octalDigits.sourceString, 8),
-      };
-      tokenList.push(token);
-    },
-    octal_digit(digit) {
-      const token = {
-        start: this.source.startIdx,
-        end: this.source.endIdx,
-        type: "octal_digit",
-        value: digit.sourceString,
-      };
-      tokenList.push(token);
-    },
-    escaped_eol(_1, _2) {
-      const token = {
-        start: this.source.startIdx,
-        end: this.source.endIdx,
-        type: "escaped_eol",
-      };
-      tokenList.push(token);
-    },
     hex_string(_1, content, _2) {
+      console.log("hex_string rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
@@ -256,16 +134,8 @@ function getTokens(text: string): Token[] {
       };
       tokenList.push(token);
     },
-    hex_digit(digit) {
-      const token = {
-        start: this.source.startIdx,
-        end: this.source.endIdx,
-        type: "hex_digit",
-        value: digit.sourceString,
-      };
-      tokenList.push(token);
-    },
-    indirect_ref(_1, objNum, _2, genNum, _3, _4) {
+    indirect_ref(objNum, _2, genNum, _3, _4) {
+      console.log("indirect_ref rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
@@ -275,54 +145,17 @@ function getTokens(text: string): Token[] {
       };
       tokenList.push(token);
     },
-    xref(_1, subsections, _2, _3) {
+    xref(_1) {
+      console.log("xref rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
         type: "xref",
-        subsections: subsections.children.map((child) => child.extract()),
       };
       tokenList.push(token);
     },
-    xref_subsection(marker, entries) {
-      const token = {
-        start: this.source.startIdx,
-        end: this.source.endIdx,
-        type: "xref_subsection",
-        marker: marker.extract(),
-        entries: entries.children.map((entry) => entry.extract()),
-      };
-      tokenList.push(token);
-    },
-    xref_subsection_marker(_1, firstObj, _2, count, _3) {
-      const token = {
-        start: this.source.startIdx,
-        end: this.source.endIdx,
-        type: "xref_subsection_marker",
-        firstObj: parseInt(firstObj.sourceString),
-        count: parseInt(count.sourceString),
-      };
-      tokenList.push(token);
-    },
-    xref_10entry(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10) {
-      const token = {
-        start: this.source.startIdx,
-        end: this.source.endIdx,
-        type: "xref_10entry",
-        value: this.sourceString,
-      };
-      tokenList.push(token);
-    },
-    xref_5entry(_1, _2, _3, _4, _5) {
-      const token = {
-        start: this.source.startIdx,
-        end: this.source.endIdx,
-        type: "xref_5entry",
-        value: this.sourceString,
-      };
-      tokenList.push(token);
-    },
-    xref_entry(tenEntry, _1, fiveEntry, _2, status, _3) {
+    xref_entry(tenEntry, _1, fiveEntry, _2, status) {
+      console.log("xref_entry rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
@@ -333,33 +166,35 @@ function getTokens(text: string): Token[] {
       };
       tokenList.push(token);
     },
-    trailer(_1, dictionary, _2, _3) {
+    trailer(_1) {
+      console.log("trailer rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
-        type: "trailer",
-        dictionary: dictionary.extract(),
+        type: "trailer"
       };
       tokenList.push(token);
     },
-    startxref(_1, position, _2, _3) {
+    startxref(_1) {
+      console.log("startxref rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
-        type: "startxref",
-        position: parseInt(position.sourceString),
+        type: "startxref"
       };
       tokenList.push(token);
     },
-    eof(_1, _2) {
+    eof(_1) {
+      console.log("eof rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
-        type: "eof",
+        type: "%%EOF",
       };
       tokenList.push(token);
     },
     comment(_1, commentText, _2) {
+      console.log("comment rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
@@ -368,64 +203,8 @@ function getTokens(text: string): Token[] {
       };
       tokenList.push(token);
     },
-    eol(content) {
-      const token = {
-        start: this.source.startIdx,
-        end: this.source.endIdx,
-        type: "eol",
-        content: content.sourceString,
-      };
-      tokenList.push(token);
-    },
-    end_delimiter(content) {
-      const token = {
-        start: this.source.startIdx,
-        end: this.source.endIdx,
-        type: "delimiter",
-        subtype: this.ctorName.toLowerCase(),
-        content: content.sourceString,
-      };
-      tokenList.push(token);
-    },
-    delimiter(content) {
-      const token = {
-        start: this.source.startIdx,
-        end: this.source.endIdx,
-        type: "delimiter",
-        subtype: this.ctorName.toLowerCase(),
-        content: content.sourceString,
-      };
-      tokenList.push(token);
-    },
-    start_delimiter(content) {
-      const token = {
-        start: this.source.startIdx,
-        end: this.source.endIdx,
-        type: "delimiter",
-        subtype: this.ctorName.toLowerCase(),
-        content: content.sourceString,
-      };
-      tokenList.push(token);
-    },
-    ws_incl_eol(content) {
-      const token = {
-        start: this.source.startIdx,
-        end: this.source.endIdx,
-        type: "whitespace",
-        inclEol: this.ctorName === "ws_incl_eol",
-      };
-      tokenList.push(token);
-    },
-    ws_no_eol(content) {
-      const token = {
-        start: this.source.startIdx,
-        end: this.source.endIdx,
-        type: "whitespace",
-        inclEol: this.ctorName === "ws_incl_eol",
-      };
-      tokenList.push(token);
-    },
     bool(value) {
+      console.log("bool rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
@@ -435,6 +214,7 @@ function getTokens(text: string): Token[] {
       tokenList.push(token);
     },
     integer(sign, digits) {
+      console.log("integer rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
@@ -446,6 +226,7 @@ function getTokens(text: string): Token[] {
       tokenList.push(token);
     },
     real(sign, part1, dot, part2) {
+      console.log("real rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
@@ -460,6 +241,7 @@ function getTokens(text: string): Token[] {
       tokenList.push(token);
     },
     null(_1) {
+      console.log("null rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
@@ -468,6 +250,7 @@ function getTokens(text: string): Token[] {
       tokenList.push(token);
     },
     name(_1, characters) {
+      console.log("name rule");
       const token = {
         start: this.source.startIdx,
         end: this.source.endIdx,
@@ -478,12 +261,22 @@ function getTokens(text: string): Token[] {
     },
   });
 
-  // const semanticsResult = semantics(matchResult);
+  // Tokenize line-by-line
+  const lines = text.split('\n');
+  let count: number = 1;
+  for (const line of lines) {
+    console.log(`Line ${count}: "${line}"`);
+    count += 1;
+    const matchResult: ohm.MatchResult = grammar.match(line);
+    if (matchResult.failed()) {
+      console.log(`getTokens() match did not succeed!`);
+    }
+    else {
+      console.log(`getTokens() match worked! ${matchResult}`);
+      semantics(matchResult).extract();
+    }
+  }
 
-  // const tokens = semantics(matchResult).extract();
-  // console.log("Extracted Tokens: ", tokens);
-  // return semanticsResult.extract();
-  semantics(matchResult).extract();
   console.log("tokenList: ", tokenList);
   return tokenList;
 }
